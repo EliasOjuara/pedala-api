f13417914767f3665242ae5fc22e24d1d045c537
EliasOjuara <araujoelias616@gmail.com>
2026-07-13 09:46:19 -0300

    iniciando bicicletas

---
+++ src/migrations/1783946719070-CriarTabelaBicicletas.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CriarTabelaBicicletas1783946719070 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
